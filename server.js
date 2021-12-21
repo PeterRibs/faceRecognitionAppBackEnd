7e09a2d80d5781e5d332f890d16a402a4517be37
PeterRibs <pedrohenriquelr@gmail.com>
2021-12-21 16:33:42 -0300

    update

--- server.js
+++ server.js
@@ -23,10 +23,7 @@ app.use(cors())
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => { res.send("it is working!") })
-app.get('/signin', (req, res) => { 
-    // signin.handleSignin(req, res, db, bcrypt) 
-    res.send("it is working!")
-})
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (res, req) => { profile.handleProfileGet(req, res, db) })
 app.put('/image', (res, req) => { image.handleImage(req, res, db) })
