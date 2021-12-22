6a366c282f3daa593ae7a533ef0409d571a8fc7a
PeterRibs <pedrohenriquelr@gmail.com>
2021-12-22 09:26:33 -0300

    update

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
@@ -22,8 +22,7 @@ const db =  knex({
 const app = express();
 
 app.use(cors())
-// app.use(bodyParser.json());
-app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => { res.send("it is working!") })
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
